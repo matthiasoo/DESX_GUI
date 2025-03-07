07559a420c26e2b3e46c9df8de30d875d55e6ac3
matthiasoo <makky.x.55@gmail.com>
2025-03-07 17:38:04 +0100

    GUI first steps

--- View/src/main/java/org/example/view/DesXApplication.java
+++ View/src/main/java/org/example/view/DesXApplication.java
@@ -1,7 +1,10 @@
 package org.example.view;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class DesXApplication extends Application {
 
@@ -11,6 +14,14 @@ public class DesXApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        Image icon = new Image("icon.png");
+
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("DESX");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
