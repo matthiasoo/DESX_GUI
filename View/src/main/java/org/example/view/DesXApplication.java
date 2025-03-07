f6161dbc1fef7e9a3fee08bcd8a01d235a92578f
matthiasoo <makky.x.55@gmail.com>
2025-03-07 16:19:15 +0100

    Project structure final form

---
+++ View/src/main/java/org/example/view/DesXApplication.java
@@ -0,0 +1,16 @@
+package org.example.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DesXApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
